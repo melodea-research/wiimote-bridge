7438d2e531f596ae17bf9a413da978f630cc502b
Alberto Di Cagno <alberto.dicagno@melodea.io>
2025-03-24 20:13:20 +0100

    fix: filename typos

--- wiimotebridged/wiimotebridged.c
+++ wiimotebridged/wiimotebridged.c
@@ -1,26 +1,14 @@
 /*
- *	wiiuse
+ *	wiimotebridged
  *
- *	Written By:
- *		Michael Laforest	< para >
- *		Email: < thepara (--AT--) g m a i l [--DOT--] com >
+ *	Author:
+ *		Alberto Di Cagno	< alberto.dicagno@melodea.io >
+
+ *      From the boilerplate by Michael Laforest < thepara@gmail.com >
  *
- *	Copyright 2006-2007
+ *	Copyright 2006-2025
  *
- *	This file is part of wiiuse.
- *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation; either version 3 of the License, or
- *	(at your option) any later version.
- *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
- *
- *	You should have received a copy of the GNU General Public License
- *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *	This file is part of Melodea 3D Interactive Visuals stack.
  *
  *	$Header$
  *
@@ -29,9 +17,7 @@
 /**
  *	@file
  *
- *	@brief Example using the wiiuse API.
- *
- *	This file is an example of how to use the wiiuse library.
+ *	@brief Bridge between WiiMote and OSC - works on Raspberry Pi chipset with BlueZ.
  */
 
 #include <stdio.h>                      /* for printf */
